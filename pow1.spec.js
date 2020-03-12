49a81a5883c351eceb2fa615c007b2ce2b39f021
Yaroslav Slepukhin <YarslavSlepukhin@gmail.com>
2020-03-13 01:46:06 +0300

    first commit

---
+++ pow1.spec.js
@@ -0,0 +1,11 @@
+describe("pow1", function () {
+
+  it("2 в степени 3 будет 8", function () {
+    assert.equal(pow1(2, 3), 8);
+  });
+
+  it("3 в степени 4 будет 81", function () {
+    assert.equal(pow1(3, 4), 81);
+  });
+
+});
