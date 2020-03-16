d4bb4fea92d7ad2038c7001551b0e0151e5684ad
Yaroslav Slepukhin <YarslavSlepukhin@gmail.com>
2020-03-16 22:32:58 +0300

    version 2.0

---
+++ main.js
@@ -0,0 +1,49 @@
+mocha.setup('bdd');
+
+const startMocha = () => {
+    document.getElementById('mocha').textContent = '';
+    window.eval(CodeMirror1.getValue());
+    window.eval(CodeMirror2.getValue());
+    window.eval(CodeMirror3.getValue());
+
+    mocha.checkLeaks();
+    mocha.run();
+};
+
+const startBenchmark = () => {
+    document.getElementById('benchmark-result').textContent = '';
+    document.getElementById('benchmark-loading').textContent = 'Loading...';
+    // document.getElementById('test').disabled = true;
+
+    let suite = new Benchmark.Suite;
+
+    suite.add('pow1', new Function("return " + CodeMirror1.getValue())())
+        .add('pow2', new Function("return " + CodeMirror2.getValue())())
+        .add('pow3', new Function("return " + CodeMirror3.getValue())())
+        .on('cycle', (event) => {
+            let result = document.createElement("p");
+            result.textContent = String(event.target);
+            document.getElementById('benchmark-result').appendChild(result);
+        })
+        .on('complete', function () {
+            let final = document.createElement("p");
+            final.classList.add('fastest');
+            final.textContent = 'Fastest is ' + this.filter('fastest').map('name');
+            document.getElementById('benchmark-result').appendChild(final);
+            document.getElementById('benchmark-loading').textContent = '';
+            // document.getElementById('test').disabled = false;
+        })
+        .run({
+            'async': true
+        });
+}
+
+const runTests = () => {
+    try {
+        startMocha();
+        startBenchmark();
+
+    } catch (err) {
+        alert('Syntax error. Try again!');
+    }
+}
---
+++ pow.spec.js
@@ -0,0 +1,35 @@
+describe("pow1", () => {
+
+  it("when raising 2 to the 3rd degree, the result is 8", function() {
+    assert.equal(pow1(2, 3), 8);
+  });
+
+  it("when raising 3 to the 4th degree is equal to 81", function() {
+    assert.equal(pow1(3, 4), 81);
+  });
+
+});
+
+describe("pow2", () => {
+
+  it("when raising 2 to the 3rd degree, the result is 8", function() {
+    assert.equal(pow1(2, 3), 8);
+  });
+
+  it("when raising 3 to the 4th degree is equal to 81", function() {
+    assert.equal(pow1(3, 4), 81);
+  });
+
+});
+
+describe("pow3", () => {
+
+  it("when raising 2 to the 3rd degree, the result is 8", function() {
+    assert.equal(pow1(2, 3), 8);
+  });
+
+  it("when raising 3 to the 4th degree is equal to 81", function() {
+    assert.equal(pow1(3, 4), 81);
+  });
+
+});
