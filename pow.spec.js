af643519e8bd1ffc7046e75b2310e4810f221c73
Yaroslav Slepukhin <YarslavSlepukhin@gmail.com>
2020-03-16 23:54:10 +0300

    fix tests bug

--- pow.spec.js
+++ pow.spec.js
@@ -13,11 +13,11 @@ describe("pow1", () => {
 describe("pow2", () => {
 
   it("when raising 2 to the 3rd degree, the result is 8", function() {
-    assert.equal(pow1(2, 3), 8);
+    assert.equal(pow2(2, 3), 8);
   });
 
   it("when raising 3 to the 4th degree is equal to 81", function() {
-    assert.equal(pow1(3, 4), 81);
+    assert.equal(pow2(3, 4), 81);
   });
 
 });
@@ -25,11 +25,11 @@ describe("pow2", () => {
 describe("pow3", () => {
 
   it("when raising 2 to the 3rd degree, the result is 8", function() {
-    assert.equal(pow1(2, 3), 8);
+    assert.equal(pow3(2, 3), 8);
   });
 
   it("when raising 3 to the 4th degree is equal to 81", function() {
-    assert.equal(pow1(3, 4), 81);
+    assert.equal(pow3(3, 4), 81);
   });
 
 });
